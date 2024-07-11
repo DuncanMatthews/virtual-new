37ab43faf84b2e57cb2dc1cb55de8dcfc464b326
Duncan <duncanmatthews15@gmail.com>
2024-07-10 21:53:37 -0700

    git push

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware((_auth, _req) => {
-  // No protection is applied, all routes are accessible
-});
+export default clerkMiddleware()
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
